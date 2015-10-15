263febd281da4a42aa5c8f6f4d8d91af5f77c15b
Jered McFerron <jeredmcferron@ccv.church>
2015-10-15 12:07:02 -0700

    Upped version number Fixed a warning with RevealBox's ElementTagMatches

--- App.Shared/Strings.cs
+++ App.Shared/Strings.cs
@@ -12,7 +12,7 @@ namespace App.Shared
             {
                 get
                 {
-                    return "Version 2.0.1 (Build 6)";
+                    return "Version 2.0.2 (Build 0)";
                 }
             }
         }
