5888cd628cb9c631919b7d282f6a30156c74bbea
Jered McFerron <jered@Jereds-MacBook-Pro.local>
2016-03-07 10:42:40 -0700

    Bumped version to 2.0.8. This will be an Android only release. Removed Location Services from the exectuable. (Via proprocesser exclusion)

--- App.Shared/Strings.cs
+++ App.Shared/Strings.cs
@@ -12,7 +12,7 @@ namespace App.Shared
             {
                 get
                 {
-                    return "Version 2.0.7 (Build 1)";
+                    return "Version 2.0.8 (Build 0)";
                 }
             }
         }
