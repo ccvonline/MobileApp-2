acc3f0869de2995ca0d26c63dae3d72618c05b28
mkinyon <mavhuntrx@gmail.com>
2016-04-12 15:28:03 -0700

    Changed the behavior of the Coach Next Step button to display a list of groups the user coaches instead of taking them directly to the toolbox.

--- App.Shared/Engagement.cs
+++ App.Shared/Engagement.cs
@@ -100,10 +100,7 @@ namespace App.Shared
         const int GroupType_NextSteps = 78;
         const int GroupTypeRole_Coach = 114;
         const int GroupTypeRole_AsstCoach = 118;
-        string [] NextStepsCoach_Entry = { "Coach", "Access your toolbox", "http://ccv.church/page/985?GroupId={0}", "coach_thumb.png" };
-
-        // Neighborhood Group Coach
-        string [] GroupCoach_Entry = { "Coach", "Access your toolbox", "http://ccv.church/page/490?GroupId={0}", "coach_thumb.png" };
+        string[] Coach_Entry = { "Coach", "Access your toolbox", "http://ccv.church/ma-my-groups", "coach_thumb.png" };
 
         // Not Coach 
         string[] NotCoach_Entry = { "Coaches", "Learn about coaching", "http://ccv.church/ma-request-coach", "coach_thumb.png" };
@@ -114,44 +111,28 @@ namespace App.Shared
             int groupId = Rock.Mobile.Util.GroupExtensions.IsMemberTypeOfGroup( groups, GroupType_NextSteps, GroupTypeRole_Coach );
             if( groupId > -1 )
             {
-                // update the entry with the groupId
-                NextStepsCoach_Entry[ 2 ] = string.Format( NextStepsCoach_Entry[ 2 ], groupId );
-
-                // yup.
-                return NextStepsCoach_Entry;
+                return Coach_Entry;
             }
 
             // second, are they a Next Steps Assistant Coach?
             groupId = Rock.Mobile.Util.GroupExtensions.IsMemberTypeOfGroup(groups, GroupType_NextSteps, GroupTypeRole_AsstCoach);
             if (groupId > -1)
             {
-                // update the entry with the groupId
-                NextStepsCoach_Entry[2] = string.Format(NextStepsCoach_Entry[2], groupId);
-
-                // yup.
-                return NextStepsCoach_Entry;
+                return Coach_Entry;
             }
 
             // then are they a Neighborhood Group Coach?
             groupId = Rock.Mobile.Util.GroupExtensions.IsMemberTypeOfGroup( groups, PrivateGeneralConfig.NeighborhoodGroupValueId, MobileAppApi.GroupMemberRole_Leader );
             if( groupId > -1 )
             {
-                // update the entry with the groupId
-                GroupCoach_Entry[ 2 ] = string.Format( GroupCoach_Entry[ 2 ], groupId );
-
-                // yup.
-                return GroupCoach_Entry;
+                return Coach_Entry;
             }
 
             // or... are they a Neighborhood Group Asst Coach?
             groupId = Rock.Mobile.Util.GroupExtensions.IsMemberTypeOfGroup(groups, PrivateGeneralConfig.NeighborhoodGroupValueId, MobileAppApi.GroupMemberRole_AsstLeader);
             if (groupId > -1)
             {
-                // update the entry with the groupId
-                GroupCoach_Entry[2] = string.Format(GroupCoach_Entry[2], groupId);
-
-                // yup.
-                return GroupCoach_Entry;
+                return Coach_Entry;
             }
 
             // they aren't either, so return the "you should do this!!!1one"
